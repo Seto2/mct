f0b29a8d5ddcec61f7f3b534d8cf16123145102f
Seto moto <seto.kami2@gmail.com>
2023-07-14 05:48:05 +0800

    hereglegchiin neriig shalgadag bolson

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,18 @@
 import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Main.css'
+import { useDispatch } from 'react-redux'
+import { setUserId } from 'redux/result_reducer'
 export default function Main() {
 
     const inputRef = useRef(null)
+    const dispatch = useDispatch()
+
+    function startQuiz(){
+        if(inputRef.current?.value){
+            dispatch(setUserId(inputRef.current?.value))
+        }
+    }
 
     return (
     <div className='container'>
@@ -21,7 +30,7 @@ export default function Main() {
         </form>
 
         <div className='start'>
-            <Link className='btn' to={'quiz'}> Start quiz </Link>
+            <Link className='btn' to={'quiz'} onClick={startQuiz}> Start quiz </Link>
         </div>
     </div>
   )
